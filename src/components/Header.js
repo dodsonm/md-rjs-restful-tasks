8701c2a3f83442af57b33388bf828188f13e17a8
dodson <matthew.dodson.dc@gmail.com>
2021-08-04 20:34:37 -0400

    Add a simple Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
+import Button from './Button';
+
 const Header = ({ title }) => {
+
+  const onClick = (e) => {
+    console.log('%s Clicked', e.target);
+  }
+
   return (
     <header>
       <h1>{title}</h1>
+      <Button text="Add" onClick={onClick} />
     </header>
   )
 }
